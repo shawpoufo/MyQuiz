00b18ce33a3e7d2f964fd756281503ee6e6d40e6
shawpoufo <azizihatim11@gmail.com>
2022-04-14 11:56:50 +0000

    adding firebase auth

--- app/src/main/java/com/example/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/quizzapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.quizzapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvSignUp;
     TextView tvGuest;
+    EditText etUsername;
+    EditText etPassword;
+    FirebaseAuth firebaseAuth;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +29,44 @@ public class MainActivity extends AppCompatActivity {
 
         tvSignUp = (TextView) findViewById(R.id.tvSignUp);
         tvGuest = findViewById(R.id.tvGuest);
-
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        firebaseAuth = FirebaseAuth.getInstance();
         tvGuest.setOnClickListener(view -> {
             startActivity(new Intent(MainActivity.this,QuizList.class));
         });
         tvSignUp.setOnClickListener(view -> {
             startActivity(new Intent(MainActivity.this,SignUp.class));
         });
+
+        btnLogin.setOnClickListener( view -> login());
+
+    }
+
+    private void login()
+    {
+        if(firebaseAuth.getCurrentUser() != null)
+        {
+            if(etUsername.getText().toString().isEmpty() || etPassword.getText().toString().isEmpty())
+                Toast.makeText(this,"Veuillez remplire tous les champs",Toast.LENGTH_SHORT).show();
+            else
+            {
+                firebaseAuth.signInWithEmailAndPassword(etUsername.getText().toString().toLowerCase(),etPassword.getText().toString().toLowerCase()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            startActivity(new Intent(MainActivity.this,QuizList.class));
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this,"UserName ou Password non valide",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/quizzapp/SignUp.java
+++ app/src/main/java/com/example/quizzapp/SignUp.java
@@ -1,21 +1,75 @@
 package com.example.quizzapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
     TextView tvLogin;
-
+    Button btnSignUp;
+    EditText tvUserName;
+    EditText tvEmail;
+    EditText tvPassword;
+    EditText tvRePassword;
+    private FirebaseAuth mAuth;
+    private final String TAG = "SIGN_UP";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
+        mAuth = FirebaseAuth.getInstance();
         tvLogin = findViewById(R.id.tvLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        tvUserName= findViewById(R.id.tvUserName);
+        tvEmail= findViewById(R.id.tvEmail);
+        tvPassword= findViewById(R.id.tvPassword);
+        tvRePassword= findViewById(R.id.tvRePassword);
 
         tvLogin.setOnClickListener(view -> startActivity(new Intent(this,MainActivity.class)));
+        btnSignUp.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
+            if(!tvEmail.getText().toString().isEmpty() && !tvPassword.getText().toString().isEmpty()
+                && !tvUserName.getText().toString().isEmpty() && !tvRePassword.getText().toString().isEmpty())
+                signUp();
+            else
+                Toast.makeText(SignUp.this, "Veuillez remplire tous les champs",
+                        Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void signUp(){
+        mAuth.createUserWithEmailAndPassword(tvEmail.getText().toString(),tvPassword.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d(TAG,"user created successfully");
+                    goToQuizList();
+                }
+                else{
+                    Log.d(TAG,"failure :"+task.getException());
+                    Toast.makeText(SignUp.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+    private void goToQuizList()
+    {
+        startActivity(new Intent(this,QuizList.class));
     }
 }
